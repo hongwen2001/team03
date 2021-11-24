5653859311f9d20669d17be0620e111c124f212c
DESKTOP-UITHU5S\F309 <d1094162060@gm.lhu.edu.tw>
2021-11-24 17:35:27 +0800

    版本正確，隨機資料未建立完成，在此之下版本都是錯誤

---
+++ database/migrations/2021_11_24_074003_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id()->unsigned()->comment('編號');
+            $table->string('student_id')->comment('學號');
+            $table->tinyInteger('seat_number')->unsigned()->comment('座號');
+            $table->string('name')->comment('姓名');
+            $table->string('gender')->comment('性別');
+            $table->tinyInteger('cid')->unsigned()->comment('班級');
+            $table->date('graduation_date')->comment('畢業年份');
+            $table->date('start_date')->comment('入學年份');
+            $table->string('seat')->comment('座位');
+            $table->string('country')->comment('出生地');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +14,5 @@
 */
 
 Route::get('/', function () {
-    return "我是第一git測試";
+    return view('welcome');
 });
